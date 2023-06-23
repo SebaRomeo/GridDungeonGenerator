76fe79b14042c71b12e48286678f608154a9f02f
Sebastian Romeo <dsromeo.dev@gmail.com>
2023-06-23 16:58:41 -0300

    added new build rule

--- src/Dungeon.cs
+++ src/Dungeon.cs
@@ -18,12 +18,13 @@
             }
         }
 
-        internal bool RoomFits(int originX, int originY, int width, int height) 
+        internal bool RoomFits(int originX, int originY, int width, int height)
         {
             int x = originX + width;
             int y = originY + height;
 
-            if (x >= grid.GetLength(0) || y >= grid.GetLength(1) || originX == 0 || originY == 0)
+            if (x >= grid.GetLength(0) || y >= grid.GetLength(1) || originX <= 1 || originY <= 1
+                || originX + width + 2 >= grid.GetLength(0) || originY + height + 2 >= grid.GetLength(1))
                 return false;
 
             for (int posX = 0; posX < width; posX++)
